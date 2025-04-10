d8c3845a41166d75505cefd17e673b69de9bd3d1
Daniel Nebdal <daniel@nebdal.net>
2025-04-10 18:36:12 +0200

    Make the multi-GPU code actually work

--- R/llm.R
+++ R/llm.R
@@ -36,31 +36,31 @@ getAndCheckEndpoint = function(endpoint_name, model) {
 }
 
 #' Ask an LLM a question about an article summary
-#' 
+#'
 #' @param endpoint_name Endpoint to send the query to
 #' @param model A model name (e.g. "phi4:latest") that's available on the endpoint
 #' @param prompt The question to ask
 #' @param summary The article summary to ask about
 #' @param verbose Print HTTP error codes if the query fails
-#' 
+#'
 #' The endpoint_name is from the settings.xml file.
-#' If you haven't changed it, the default "localhost" 
+#' If you haven't changed it, the default "localhost"
 #' points to http://localhost:11434, the default for ollama.
-#' 
+#'
 #' The valid model names are fetched when initially reading the settings.
 #' If you have installed a new model later, re-run publlamaInit().
-#' 
+#'
 #' @export
 askLLM = function(model, prompt, summary, endpoint_name="localhost", verbose=TRUE) {
   URL = getAndCheckEndpoint(endpoint_name, model)
-  
+
   if (length(grep("%s", prompt)) == 0) {
     # Glue on a %s if the prompt doesn't have one
     # There are ways to make this fail, but it'll
     # cover the common "the question is plain English" case.
     prompt = sprintf("%s\n%%s", prompt)
   }
-  
+
   reqbody = list(
     prompt = sprintf(prompt, summary),
     model  = model,
@@ -96,7 +96,7 @@ askRaw = function(URL, model, query, verbose=FALSE) {
     model  = model,
     stream = FALSE
   )
-  
+
   req <- httr2::request(sprintf("%s/generate", URL)) |>
     httr2::req_headers("Content-Type" = "application/json") |>
     httr2::req_body_json(reqbody) |>
@@ -111,68 +111,70 @@ askRaw = function(URL, model, query, verbose=FALSE) {
     retries = retries - 1
     if(retries == 0) {return(NA)}
     Sys.sleep(stats::runif(1)*sleepmax + sleepbase)
-    
+
     resp = httr2::req_perform(req)
   }
-  
+
   answer = httr2::resp_body_json(resp)$response
   return(answer)
-  
+
 }
 
 #' Ask an LLM a question about an article summary
-#' 
+#'
 #' @param model Model name(s) to ask (must be available on all endpoints)
 #' @param prompt Prompt name defined in the settings
 #' @param article A data frame of one or more articles to ask about
 #' @param endpoint Endpoint to send the query to
 #' @param verbose Print HTTP error codes if the query fails
-#' 
+#'
 #' The endpoint_name is from the settings.xml file.
-#' If you haven't changed it, the default "localhost" 
+#' If you haven't changed it, the default "localhost"
 #' points to http://localhost:11434, the default for ollama.
-#' 
+#'
 #' The valid model names are fetched when initially reading the settings.
 #' If you have installed a new model later, re-run publlamaInit().
-#' 
+#'
 #' @export
-askLLMVec = function(model, prompt, article, endpoint="localhost", verbose=TRUE) {
-  perror = function(str, ...) { write(sprintf(str, ...), stderr()) }
+askLLMVec = function(model, prompt, article, endpoint="localhost", verbose=FALSE) {
+  Nw = length(endpoint)
+  oldPlan = future::plan(future::multicore, workers=Nw)
   
-  work = expand.grid(pmid=article$pmid, prompt=prompt, model=model)
   epMod = expand.grid(endpoint=endpoint, model=model)
   epMod$URL = apply(epMod, 1, function (r){ getAndCheckEndpoint(r[1], r[2]) })
-  work = merge(work, epMod)
+
+  work = expand.grid(pmid=article$pmid, prompt=prompt, model=model)
   work = merge(work, article)
-  work$endpoint = endpoint[ 1+(1:nrow(work) %% length(endpoint)) ]
-  
+  work$URL = epMod$URL[ 1+(1:nrow(work) %% Nw) ]
+
   promptCache = lapply(prompt, function(p){ getPrompt(p)$prompt })
   names(promptCache) = prompt
+  p = progressr::progressor(nrow(work))
   
-  res=NA
-  if(verbose) {
-    printf("Parallel: %d threads\n", length(endpoint))
-    res = parallel::mclapply(1:nrow(work), function(i) {
-      perror("[% 8d] %s /  %s", Sys.getpid(), work$URL[i], work$model[i])
-      query = promptCache[[ work$prompt[i] ]]
-      abstract = article$summary[i]
-      question = sprintf(query, abstract)
-      list(question, askRaw(work$URL[i], work$model[i], question, TRUE))
-    }, mc.cores=1, mc.preschedule=T, mc.silent = F)
-  } else {
-    res = pbmcapply::pbmclapply(
-      1:nrow(work), 
-      mc.cores=length(endpoint), 
-      mc.preschedule=T,
-      FUN = function(i) {
-        query = promptCache[[ work$prompt[i] ]]
-        abstract = article$summary[i]
-        question = sprintf(query, abstract)
-        
-        list(question, askRaw(work$URL[i], work$model[i], question, FALSE))
+  res = future.apply::future_lapply( endpoint, function(e) {
+    myWork = subset(work, endpoint==e)
+    res = lapply(1:nrow(myWork), function(i) {
+      row = myWork[i,]
+      query = promptCache[[ row$prompt ]]
+      abstract = row$summary
+      question = sprintf(query, row$summary)
+      answer=askRaw(row$URL, row$model, question, FALSE)
+      if(verbose) {
+        p(sprintf("[% 20s] prompt %s, pmid %s, model %s", row$URL, row$prompt, row$pmid, row$model))
+      } else {
+        p()
+      }
+      return(data.frame(
+        pmid=row$pmid,
+        model=row$model,
+        prompt=row$prompt,
+        answer=answer
+      ))
     })
-    
-  }
+    return(do.call(rbind, res))
+  })
   
-  return(list(work=work, answers=res))
+  res = merge(work, do.call(rbind, res))
+  future::plan(oldPlan)
+  return( res )
 }
